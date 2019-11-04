b38ad548b10325a86bf2925cabcbc31e9bef5284
HungQuocLam <quochung11@gmail.com>
2019-11-03 21:25:08 -0800

    Update

--- src/elapse_timer.h
+++ src/elapse_timer.h
@@ -3,39 +3,31 @@
 #include <ctime>
 #include <cmath>
 
-class Elapse_timer
-{
+class Elapse_timer{
 public:
-    void start()
-    {
+    void start(){
         m_StartTime = std::chrono::system_clock::now();
         m_bRunning = true;
     }
     
-    void stop()
-    {
+    void stop(){
         m_EndTime = std::chrono::system_clock::now();
         m_bRunning = false;
     }
     
-    double elapsedMilliseconds()
-    {
+    double elapsedMilliseconds(){
         std::chrono::time_point<std::chrono::system_clock> endTime;
-        
-        if(m_bRunning)
-        {
+        if(m_bRunning){
             endTime = std::chrono::system_clock::now();
         }
-        else
-        {
+        else{
             endTime = m_EndTime;
         }
         
         return std::chrono::duration_cast<std::chrono::milliseconds>(endTime - m_StartTime).count();
     }
     
-    double elapsedSeconds()
-    {
+    double elapsedSeconds(){
         return elapsedMilliseconds() / 1000.0;
     }
 
